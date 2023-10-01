3d3682cdad1d01149b83501786a09e9bfdbff3e5
wildercb <73315944+wildercb@users.noreply.github.com>
2023-09-30 21:17:53 -0400

    Update Maine.js

--- Maine.js
+++ Maine.js
@@ -1,5 +1,33 @@
-var marqBounds;
+document.getElementsByClassName("topCon").style.borderWidth = "thin"; 
 
-document.getElementById('topDiv').innerHTML = marqBounds;
 
-console.log(marqBounds)
+function MouseRollover(myImage) {
+    myImage.src = 'images/Pinoch.png';
+}
+
+function MouseOut(myImage){
+    myImage.src = 'images/Pinoch.jpg';
+}
+
+function MouseRoll2(topCon){
+    document.getElementById("topCon").style.borderWidth = "thick";    
+}
+
+function MouseOut2(topCon){
+    document.getElementById("topCon").style.borderWidth = "thin";    
+}
+
+function MouseRoll3(topCon2){
+    document.getElementById("topCon2").style.borderWidth = "thick";    
+}
+
+function MouseOut3(topCon2){
+    document.getElementById("topCon2").style.borderWidth = "thin";    
+}
+function MouseRoll4(topCon3){
+    document.getElementById("topCon3").style.borderWidth = "thick";    
+}
+
+function MouseOut4(topCon3){
+    document.getElementById("topCon3").style.borderWidth = "thin";    
+}
